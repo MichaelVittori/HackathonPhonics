a25793713e0ac85400271bad0805c5d121b50031
MichaelVittori <112675358+MichaelVittori@users.noreply.github.com>
2024-02-17 14:00:04 -0500

    Update server.py

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,8 +1,10 @@
+# Running this file will start the server on port 5000
+
 from flask import Flask
 
 app = Flask(__name__)
 
-
+# Navigate to localhost:5000/home to access
 @app.route("/home")
 def home():
     return "hello!"
